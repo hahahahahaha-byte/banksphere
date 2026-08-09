bb46a8934c1930e00fd19a886d15edde9d61377a
hahahahahaha-byte <zaroon3013@gmail.com>
2026-08-09 16:17:46 +0500

    Update for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/banksphere/',   // must match your repo name exactly
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
